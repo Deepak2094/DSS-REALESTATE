7935625cab9c63e4fac1a2779b925f6b413d48c0
Vishnu Deepak <47251994+Deepak2094@users.noreply.github.com>
2020-11-29 20:03:21 -0500

    11/29 update

---
+++ Deliverables/API_Call/Realtor_API.py
@@ -0,0 +1,142 @@
+import requests
+import json
+import glob
+import pandas as pd
+
+Data = {}
+url = "https://realtor.p.rapidapi.com/properties/v2/list-for-sale"
+
+for count in range(5):
+    offset = count * 200  # Paging Feature, offsets the data by 200 rows
+    querystring = {"sort": "relevance", "city": "Arlington", "limit": "200", "offset": "{0}".format(offset),
+                   "state_code": "VA"}
+
+    headers = {
+        'x-rapidapi-host': "realtor.p.rapidapi.com",
+        'x-rapidapi-key': "ede51666fcmsh9785fa150d39b75p1787e9jsnf48a84ac05c4"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    temp = json.loads(response.text)
+    for k, v in temp.items():
+        Data[k] = v
+
+    with open(f'Test{count}.json', 'w') as outfile:
+        json.dump(Data, outfile)
+
+Data_Dictionary = {}
+agent_name = []
+agent_office = []
+property_ID = []
+Listing_ID = []
+Listing_Url = []
+prop_type = []
+prop_sub_type = []
+city = []
+Line = []
+postal_code = []
+lat = []
+lon = []
+neighborhood_name = []
+price = []
+baths_half = []
+baths_full = []
+baths = []
+beds = []
+size = []
+thumbnail = []
+lot_size = []
+
+# Opening the file and filtering for the data we need
+for f in glob.glob("Test*.json"):
+    with open(f, "r") as read_file:
+        Data = json.load(read_file)
+        for k, v in Data.items():
+            if k == "properties":
+
+                for i in range(len(v)):
+                    property_ID.append(Data["properties"][i]["property_id"])
+                    Listing_ID.append(Data["properties"][i]["listing_id"])
+                    Listing_Url.append(Data["properties"][i]["rdc_web_url"])
+                    prop_type.append(Data["properties"][i]["prop_type"])
+
+                    if "prop_sub_type" in Data["properties"][i].keys():
+                        prop_sub_type.append(Data["properties"][i]["prop_sub_type"])
+                    else:
+                        prop_sub_type.append("NA")
+
+                    if "lot_size" in Data["properties"][i].keys():
+                        lot_size.append(Data["properties"][i]["lot_size"]["size"])
+                    else:
+                        lot_size.append("NA")
+
+                    if "agents" in Data["properties"][i].keys():
+                        agent_name.append(Data["properties"][i]["agents"][0]["name"])
+                    else:
+                        agent_name.append("NA")
+
+                    if "office" in Data["properties"][i].keys():
+                        agent_office.append(Data["properties"][i]["office"]["name"])
+                    else:
+                        agent_office.append("NA")
+
+                    city.append(Data["properties"][i]["address"]["city"])
+                    Line.append(Data["properties"][i]["address"]["line"])
+                    postal_code.append(Data["properties"][i]["address"]["postal_code"])
+
+                    lat.append(Data["properties"][i]["address"]["lat"])
+                    lon.append(Data["properties"][i]["address"]["lon"])
+
+                    if "neighborhood_name" in Data["properties"][i]["address"].keys():
+                        neighborhood_name.append(Data["properties"][i]["address"]["neighborhood_name"])
+                    else:
+                        neighborhood_name.append("NA")
+
+                    price.append(Data["properties"][i]["price"])
+
+                    if "baths_half" in Data["properties"][i].keys():
+                        baths_half.append(Data["properties"][i]["baths_half"])
+                    else:
+                        baths_half.append("NA")
+
+                    if "baths_full" in Data["properties"][i].keys():
+                        baths_full.append(Data["properties"][i]["baths_full"])
+                    else:
+                        baths_full.append("NA")
+
+                    baths.append(Data["properties"][i]["baths"])
+                    beds.append(Data["properties"][i]["beds"])
+
+                    if "building_size" in Data["properties"][i].keys():
+                        size.append(Data["properties"][i]["building_size"]["size"])
+                    else:
+                        size.append("NA")
+
+                    if "thumbnail" in Data["properties"][i].keys():
+                        thumbnail.append(Data["properties"][i]["thumbnail"])
+                    else:
+                        thumbnail.append("NA")
+
+Data_Dictionary["agent_name"] = agent_name
+Data_Dictionary["agent_office"] = agent_office
+Data_Dictionary["property_ID"] = property_ID
+Data_Dictionary["Listing_ID"] = Listing_ID
+Data_Dictionary["price"] = price
+Data_Dictionary["baths_half"] = baths_half
+Data_Dictionary["baths_full"] = baths_full
+Data_Dictionary["baths"] = baths
+Data_Dictionary["beds"] = beds
+Data_Dictionary["size_in_sqft"] = size
+Data_Dictionary["lot_size_in_sqft"] = lot_size
+Data_Dictionary["prop_type"] = prop_type
+Data_Dictionary["prop_sub_type"] = prop_sub_type
+Data_Dictionary["city"] = city
+Data_Dictionary["Line"] = Line
+Data_Dictionary["postal_code"] = postal_code
+Data_Dictionary["lat"] = lat
+Data_Dictionary["lon"] = lon
+Data_Dictionary["neighborhood_name"] = neighborhood_name
+Data_Dictionary["Listing_Url"] = Listing_Url
+Data_Dictionary["thumbnail"] = thumbnail
+
+df = pd.DataFrame(Data_Dictionary)
+df.to_csv("Real_Estate_Listings_Test.csv", index=False)
---
+++ Deliverables/Modules/format_Listings.py
@@ -0,0 +1,51 @@
+""" This file conatins the user defined function to format the Listings for Landing Page """
+import random
+import pandas as pd
+
+
+def random_phone_num_generator():
+    first = str(random.randint(100, 999))
+    second = str(random.randint(1, 888)).zfill(3)
+    last = (str(random.randint(1, 9998)).zfill(4))
+    while last in ['1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888']:
+        last = (str(random.randint(1, 9998)).zfill(4))
+    return '{}-{}-{}'.format(first, second, last)
+
+
+Agent_data = pd.read_csv("../Inputs/Agents.csv")
+
+
+def random_agent(df):
+    agent_name = random.choice(df["agent_name"].tolist())
+    agent_office = random.choice(df["agent_office"].tolist())
+    return [agent_name, agent_office]
+
+
+def format_Listings(Listings_dict):
+    variable_dict = {}
+    for i in range(1, 7):
+        variable_dict[f"Rank{i}_thumbnail"] = Listings_dict["thumbnail"][i]
+        variable_dict[f"Rank{i}_agent_name"] = random_agent(Agent_data)[0]
+        variable_dict[f"Rank{i}_agent_office"] = random_agent(Agent_data)[1]
+        variable_dict[f"Rank{i}_phnumber"] = random_phone_num_generator()
+        variable_dict[f"Rank{i}_Price"] = "{:,}".format(int(Listings_dict["price"][i]))
+        variable_dict[f"Rank{i}_beds"] = int(Listings_dict["beds"][i])
+        variable_dict[f"Rank{i}_baths"] = int(Listings_dict["baths"][i])
+        variable_dict[f"Rank{i}_size"] = "{:,}".format(int(Listings_dict["size_in_sqft"][i]))
+        variable_dict[f"Rank{i}_address"] = Listings_dict["Line"][i]
+        variable_dict[f"Rank{i}_School"] = Listings_dict["NearestSchools"][i]
+        variable_dict[f"Rank{i}_Metro"] = (
+                Listings_dict["NearestMetro"][i] + "-" + str(round(Listings_dict["MetroDistance"][i], 1)))
+        variable_dict[f"Rank{i}_BusStop"] = round(Listings_dict["BusDistance"][i], 1)
+        variable_dict[f"Rank{i}_CommunityCenter"] = Listings_dict["NearestCC"][i]
+        variable_dict[f"Rank{i}_Library"] = Listings_dict["NearestLIB"][i]
+        variable_dict[f"Rank{i}_Restaurant"] = ','.join(
+            [str(elem) for elem in Listings_dict["Top_5_Rated_restaurants"][i].split(';')[0:3]])
+
+        variable_dict[f"Rank{i}_Bars"] = ','.join(
+            [str(elem) for elem in Listings_dict["Top_5_Rated_bars"][i].split(';')[0:3]])
+       # variable_dict[f"Rank{i}_MoreRestaurants"] = ','.join(
+        #     [str(elem) for elem in Listings_dict["Top_5_Rated_restaurants"][i].split(';')[1:]])
+        # variable_dict[f"Rank{i}_MoreBars"] = ','.join(
+        #     [str(elem) for elem in Listings_dict["Top_5_Rated_bars"][i].split(';')[1:]])
+    return variable_dict
